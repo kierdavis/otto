93fec404fdef93715a6ab348c7206dfdace47afc
Kier Davis <me@kierdavis.com>
2026-05-12 00:58:35 +0100

    improve timing

--- src/midi.rs
+++ src/midi.rs
@@ -1,10 +1,17 @@
 use crate::music_theory::Pitch;
+use alsa::poll::{Descriptors, poll, pollfd};
 use alsa::seq::{self, EvNote, EventType, PortCap, PortType, Seq};
+use std::time::{Duration, Instant};
+
+fn poll_descriptors<'a>(seq: &'a Seq) -> impl Descriptors + 'a {
+  (seq, Some(alsa::Direction::Capture))
+}
 
 pub struct Interface {
   seq: Seq,
   sync_port_id: i32,
   output_port_id: i32,
+  pollfds: Vec<pollfd>,
 }
 
 impl Interface {
@@ -36,24 +43,59 @@ impl Interface {
       )
       .expect("failed to create ALSA sequencer output port");
 
+    let pollfds = poll_descriptors(&seq)
+      .get()
+      .expect("failed to get pollfds for ALSA sequence client");
+
     Self {
       seq,
       sync_port_id,
       output_port_id,
+      pollfds,
     }
   }
 
-  pub fn try_recv(&self) -> Option<Event> {
-    const EAGAIN: i32 = 11;
+  pub fn recv_timeout(&mut self, timeout: Duration) -> Option<Event> {
+    self.recv_deadline(Instant::now() + timeout)
+  }
+
+  pub fn recv_deadline(&mut self, deadline: Instant) -> Option<Event> {
     let mut input = self.seq.input();
+
     loop {
-      match input.event_input() {
-        Ok(ev) => match ev.get_type() {
-          EventType::Clock if ev.get_dest().port == self.sync_port_id => return Some(Event::Clock),
-          _ => {}
-        },
-        Err(err) if err.errno() == EAGAIN => return None,
-        Err(err) => panic!("failed to receive ALSA sequencer event: {err}"),
+      let now = Instant::now();
+      if now >= deadline {
+        return None;
+      }
+
+      let userspace_buffer_is_empty = input
+        .event_input_pending(false)
+        .expect("failed to query ALSA sequencer client for buffered events")
+        == 0;
+      if userspace_buffer_is_empty {
+        let timeout_ms = deadline.duration_since(now).as_millis().try_into().unwrap();
+        match poll(&mut self.pollfds, timeout_ms) {
+          Ok(0) => return None, // Timed out.
+          Ok(_) => {}           // Maybe there's an event to read, or the fd is closed.
+          Err(err) => panic!("failed to poll ALSA sequencer for events: {err}"),
+        }
+
+        use alsa::poll::Flags;
+        let revents = poll_descriptors(&self.seq)
+          .revents(&self.pollfds)
+          .expect("failed to extract revents from ALSA sequencer poll response");
+        if !revents.contains(Flags::IN) {
+          panic!("ALSA sequencer poll returned {revents:?}");
+        }
+      }
+
+      // If we get here, there's an event ready to read - either from the userspace buffer or from the kernel.
+      let ev = input
+        .event_input()
+        .expect("failed to read event from ALSA sequencer");
+      match ev.get_type() {
+        EventType::Clock if ev.get_dest().port == self.sync_port_id => return Some(Event::Clock),
+        _ => {} // Event ignored.
       }
     }
   }
--- src/realtime.rs
+++ src/realtime.rs
@@ -3,7 +3,7 @@ use crate::midi;
 use crossbeam::channel::{self, Receiver, Sender, TryRecvError};
 use std::sync::OnceLock;
 use std::thread::sleep;
-use std::time::{Duration, Instant};
+use std::time::Instant;
 
 pub fn main() {
   let (datamodel_changes_sender, datamodel_changes) = channel::bounded(32);
@@ -18,8 +18,6 @@ pub fn main() {
   })
   .main()
 }
-
-const MAX_BASE_TICK_INTERVAL: Duration = Duration::from_millis(50);
 
 struct Realtime {
   midi_iface: midi::Interface,
@@ -38,42 +36,33 @@ impl Realtime {
   }
 
   fn main_loop_builtin_clock(&mut self) {
-    let bpm = 140;
-    let beat_interval = Duration::from_micros(60_000_000 / bpm);
+    let beat_rate = ClockRate::per_minute(140);
+    let note_rate = beat_rate * 2;
+    let toggle_indicator_rate = beat_rate * 2;
 
-    // Ticks need to be as frequent as double the BPM, since that's how often we toggle the clock indicator.
-    // Ticks also need to be sufficiently frequent that we service idle interactions often enough.
-    let mut ticks_per_beat = 2;
-    while beat_interval / ticks_per_beat > MAX_BASE_TICK_INTERVAL {
-      ticks_per_beat *= 2;
-    }
+    let tick_rate = ClockRate::divisible_into(note_rate, toggle_indicator_rate);
+    let tick_rate = tick_rate.as_fast_as(min_poll_idle_tasks_rate());
 
-    let tick_interval = beat_interval / ticks_per_beat;
-    let mut beat_divider = ClockDivider::new(ticks_per_beat);
-    let mut toggle_indicator_divider = ClockDivider::new(ticks_per_beat / 2);
+    let mut note_divider = note_rate.new_divider_relative_to(tick_rate);
+    let mut toggle_indicator_divider = toggle_indicator_rate.new_divider_relative_to(tick_rate);
 
+    let tick_interval = tick_rate.interval();
     let mut next_tick = Instant::now() + tick_interval;
 
     loop {
       let now = Instant::now();
       if next_tick > now {
-        match self.service_one_idle_task() {
-          NothingToService => {
-            // Just sleep until the next tick.
-            sleep(next_tick - now);
-          }
-          ConsumedTime => {
-            // Check the wallclock again.
-            continue;
-          }
+        match self.service_idle_tasks() {
+          NothingToService => sleep(next_tick - now),
+          TimeElapsed => continue, // Next time might already be overdue.
           RestartMainLoop => return,
         }
       }
 
       // Tick occurs now.
       debug_assert!(next_tick <= Instant::now());
-      if beat_divider.tick() {
-        self.beat();
+      if note_divider.tick() {
+        self.advance_automaton();
       }
       if toggle_indicator_divider.tick() {
         Change::ToggleClockIndicator.apply();
@@ -84,36 +73,38 @@ impl Realtime {
 
   fn main_loop_midi_clock(&mut self) {
     let ticks_per_beat = 24; // standard
-    let mut beat_divider = ClockDivider::new(ticks_per_beat);
+    let notes_per_beat = 2;
+    let ticks_per_note = ticks_per_beat / notes_per_beat;
+    let mut note_divider = ClockDivider::new(ticks_per_note);
     let mut toggle_indicator_divider = ClockDivider::new(ticks_per_beat / 2);
 
+    let recv_timeout = min_poll_idle_tasks_rate().interval();
+
     loop {
-      match self.midi_iface.try_recv() {
+      match self.midi_iface.recv_timeout(recv_timeout) {
         Some(midi::Event::Clock) => {
           // Tick occurs now.
-          if beat_divider.tick() {
-            self.beat();
+          if note_divider.tick() {
+            self.advance_automaton();
           }
           if toggle_indicator_divider.tick() {
             Change::ToggleClockIndicator.apply();
           }
         }
-        None => match self.service_one_idle_task() {
-          NothingToService => {
-            sleep(MAX_BASE_TICK_INTERVAL);
-          }
-          ConsumedTime => {}
-          RestartMainLoop => return,
-        },
+        None => {}
+      }
+      match self.service_idle_tasks() {
+        NothingToService | TimeElapsed => {}
+        RestartMainLoop => return,
       }
     }
   }
 
-  fn service_one_idle_task(&mut self) -> ServiceIdleTaskResult {
+  fn service_idle_tasks(&mut self) -> ServiceIdleTasksResult {
     if self.pending_precompute_next_automaton_state {
       datamodel::automaton_state().next();
       self.pending_precompute_next_automaton_state = false;
-      return ConsumedTime;
+      return TimeElapsed;
     }
     loop {
       match self.datamodel_changes.try_recv() {
@@ -127,39 +118,23 @@ impl Realtime {
     NothingToService
   }
 
-  fn beat(&self) {
+  fn advance_automaton(&self) {
     use crate::music_theory::{Pitch, Scale};
-    let x_scale = Scale([
+    let scale = Scale([
       Pitch::from_midi(38),
-      Pitch::from_midi(40),
       Pitch::from_midi(41),
       Pitch::from_midi(43),
       Pitch::from_midi(45),
-      Pitch::from_midi(46),
       Pitch::from_midi(48),
       Pitch::from_midi(50),
-      Pitch::from_midi(52),
-    ]);
-    let y_scale = Scale([
-      Pitch::from_midi(62),
-      Pitch::from_midi(64),
-      Pitch::from_midi(65),
-      Pitch::from_midi(67),
-      Pitch::from_midi(69),
-      Pitch::from_midi(70),
-      Pitch::from_midi(72),
-      Pitch::from_midi(74),
-      Pitch::from_midi(76),
+      Pitch::from_midi(53),
+      Pitch::from_midi(55),
+      Pitch::from_midi(57),
     ]);
 
     let old_state = datamodel::automaton_state();
     let (_, bounces) = old_state.next();
     for &bounce in bounces {
-      use crate::automaton::Heading::*;
-      let scale = match bounce.wall {
-        PosX | NegX => &x_scale,
-        PosY | NegY => &y_scale,
-      };
       let pitch = scale.at(bounce.coord_along_wall);
       self.midi_iface.send_note_off(pitch);
       self.midi_iface.send_note_on(pitch);
@@ -169,13 +144,13 @@ impl Realtime {
   }
 }
 
-enum ServiceIdleTaskResult {
+enum ServiceIdleTasksResult {
   NothingToService,
-  ConsumedTime,
+  TimeElapsed,
   RestartMainLoop,
 }
 
-use ServiceIdleTaskResult::*;
+use ServiceIdleTasksResult::*;
 
 static DATAMODEL_CHANGES_SENDER: OnceLock<Sender<Change>> = OnceLock::new();
 
@@ -187,18 +162,78 @@ pub fn on_datamodel_change(change: Change) {
     .expect("realtime thread crashed");
 }
 
+fn min_poll_idle_tasks_rate() -> ClockRate {
+  ClockRate::per_second(10)
+}
+
 struct ClockDivider {
-  divisor: u32,
-  pos: u32,
+  divisor: u64,
+  pos: u64,
 }
 
 impl ClockDivider {
-  fn new(divisor: u32) -> Self {
+  fn new(divisor: u64) -> Self {
     Self { divisor, pos: 0 }
   }
   fn tick(&mut self) -> bool {
     let result = self.pos == 0;
     self.pos = (self.pos + 1) % self.divisor;
     result
+  }
+}
+
+use clock_rate::ClockRate;
+mod clock_rate {
+  use super::ClockDivider;
+  use num::{
+    integer::{gcd, lcm},
+    rational::Ratio,
+  };
+  use std::ops::Mul;
+  use std::time::Duration;
+
+  #[derive(Clone, Copy, Debug)]
+  pub struct ClockRate(Ratio<u64>);
+
+  impl ClockRate {
+    pub fn per_second(x: u64) -> Self {
+      Self(Ratio::new(x, 1))
+    }
+
+    pub fn per_minute(x: u64) -> Self {
+      Self(Ratio::new(x, 60))
+    }
+
+    pub fn divisible_into(r1: Self, r2: Self) -> Self {
+      Self(Ratio::new(
+        lcm(*r1.0.numer(), *r2.0.numer()),
+        gcd(*r1.0.denom(), *r2.0.denom()),
+      ))
+    }
+
+    pub fn as_fast_as(self, minimum: Self) -> Self {
+      Self(self.0 * rational_to_integer_ceil(minimum.0 / self.0))
+    }
+
+    pub fn interval(self) -> Duration {
+      Duration::from_micros((1_000_000 * *self.0.denom()) / *self.0.numer())
+    }
+
+    pub fn new_divider_relative_to(self, base: Self) -> ClockDivider {
+      let divisor = base.0 / self.0;
+      assert_eq!(*divisor.denom(), 1);
+      ClockDivider::new(*divisor.numer())
+    }
+  }
+
+  impl Mul<u64> for ClockRate {
+    type Output = Self;
+    fn mul(self, multiplier: u64) -> Self {
+      Self(self.0 * multiplier)
+    }
+  }
+
+  fn rational_to_integer_ceil(r: Ratio<u64>) -> u64 {
+    (*r.numer() + *r.denom() - 1) / *r.denom()
   }
 }
